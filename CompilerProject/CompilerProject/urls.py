b45ac48b8c2c961e9a12d101b12e3a6dd5db90d7
liamhealy <liam.healy1@marist.edu>
2019-01-24 18:53:05 -0500

    Updated build for Project-One

--- CompilerProject/CompilerProject/urls.py
+++ CompilerProject/CompilerProject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from compiler import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
