5c6a395ee057d154cf3246bc57e4713d16bc3e11
liamhealy <liam.healy1@marist.edu>
2019-01-31 11:04:26 -0500

    Updating language and development environment

--- README.md
+++
@@ -1,4 +0,0 @@
-# CMPT432N-PythonCompiler
-Design of Compilers - Dr. Labouseur - Spring 2019
-
-This is the directory for Project One.
--- environment/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\lheal\documents\github\cmpt432n-pythoncompiler\environment\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
