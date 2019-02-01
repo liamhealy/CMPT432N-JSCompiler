ed7f1f33e69351996e2239cc538e444f61631d09
liamhealy <liam.healy1@marist.edu>
2019-02-01 18:03:31 -0500

    Added global variables, functions

---
+++ scripts/lexer.js
@@ -0,0 +1,10 @@
+/*  lexer.js    */
+
+function lex() {
+    // Grab the "raw" source code.
+    var sourceCode = document.getElementById("taSourceCode").value;
+    // Trim the leading and trailing spaces.
+    sourceCode = trim(sourceCode);
+    // TODO: remove all spaces in the middle; remove line breaks too.
+    return sourceCode;
+}
