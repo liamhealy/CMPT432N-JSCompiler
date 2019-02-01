1fb565ad5662f132a693cf794f679730e91c070a
liamhealy <liam.healy1@marist.edu>
2019-02-01 18:14:47 -0500

    Update HTML and CSS

--- scripts/lexer.js
+++ scripts/lexer.js
@@ -2,7 +2,7 @@
 
 function lex() {
     // Grab the "raw" source code.
-    var sourceCode = document.getElementById("taSourceCode").value;
+    var sourceCode = document.getElementById("input").value;
     // Trim the leading and trailing spaces.
     sourceCode = trim(sourceCode);
     // TODO: remove all spaces in the middle; remove line breaks too.
