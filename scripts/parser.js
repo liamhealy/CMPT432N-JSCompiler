0cd3749a3ddd49d949342e8f0c19eee54e00f0f2
liamhealy <liam.healy1@marist.edu>
2019-03-06 16:06:54 -0500

    Update parser.js
    -Created a skeleton for the parser, including foreseeable necessary functions and variables.

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,1 +1,29 @@
 // parser.js -- where parsing and tree creation will take place
+
+var expectedType = "";
+
+function getToken() {
+    currentToken = tokenSequence[0];
+}
+
+function parseStart() {
+
+}
+
+function parseValue() {
+
+}
+
+function parseExpr() {
+
+}
+
+function parseValues() {
+
+}
+
+function match(expectedType) {
+    if (currentToken != expectedType) {
+        // Ultimately, parsing failure wil occur.
+    }
+}
