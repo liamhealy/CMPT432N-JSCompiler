dd91e0f743a965a02c30b9f5d778f1b47d14a5c8
liamhealy <liam.healy1@marist.edu>
2019-03-05 19:59:21 -0500

    Updated index.html & utils.js
    -Added warning element to index.html for users' sake.
    -Using the File Reader API,
     created solution for uploading input via local files.(https://developer.mozilla.org/en-US/docs/Web/API/FileReader)

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,6 +4,9 @@
     Utility functions.
 */
 
+const selectedFile = document.getElementById("upload").addEventListener('change', getFile);
+var fileReader = new FileReader();
+
 function trim(str)      // Use a regular expression to remove leading and trailing spaces.
 {
 	return str.replace(/^\s+ | \s+$/g, "");
@@ -61,3 +64,26 @@ function matchBreak(str) {
 //         return false;
 //     }
 // }
+
+//  The File Reader API https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+
+function getFile(event) {
+    // fileReader.onload = 
+    const uploadedFile = event.target;
+    if ('files' in uploadedFile && uploadedFile.files.length > 0) {
+        insertContent(document.getElementById("input"), uploadedFile.files[0])
+    }
+}
+
+function insertContent(target, file) {
+    readContent(file).then(content => { target.value = content}).catch(error => console.log,log(error))
+}
+
+function readContent(file) {
+    const reader = new FileReader()
+    return new Promise((resolve, reject) => {
+        reader.onload = event => resolve(event.target.result)
+        reader.onerror = error => reject(error)
+        reader.readAsText(file)
+    })
+}
