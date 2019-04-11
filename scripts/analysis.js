193d1d6e401fd3b392732c104b5fa2a86ebe581e
liamhealy <liam.healy1@marist.edu>
2019-04-11 15:07:32 -0400

    Update analysis.js
    -Started the bsaic setup for creation of the AST.

--- scripts/analysis.js
+++ scripts/analysis.js
@@ -5,6 +5,31 @@
  * will update when I decide.
  */
 
- function analysis() {
-    putMessage("\nSemantic analysis + symbol table display will occur here...");
- }
+
+// Set up error and warning counters
+var semErrors = 0;
+
+var semWarnings = 0;
+
+// Need to build an AST before a symbol table
+var ast = new Tree();
+
+ast.addNode("Root", "branch");
+
+// Reset the sequence index and current token indicator
+// using the variables from parser.js
+sequenceIndex = 0;
+
+thisToken = tokenSequence[sequenceIndex];
+
+// Needed to keep track of the scope
+var scopeLevel = 0;
+
+function analysis() {
+   putMessage("\nSemantic analysis + symbol table display will occur here...");
+
+   ast.addNode("Program", "branch");
+}
+
+// Non-terminals and terminals will be handled down here
+
